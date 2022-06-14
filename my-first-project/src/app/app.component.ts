d8fb33c50eaf06df7cc160d97f9aa6ab05045303
martins20 <martins20.tech@gmail.com>
2022-06-13 21:25:49 -0300

    feat(ngOnInit): life cicle to be called first of all methods from component class

--- my-first-project/src/app/app.component.ts
+++ my-first-project/src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  // It will be called when component render into client screen
+  ngOnInit(): void {
+    console.log("I'll be the first");
+  }
+}
